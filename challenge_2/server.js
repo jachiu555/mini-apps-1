6ed06992555f64b1108a7eb1aa86c2a2f08588f2
James Chiu <jachiu555@gmail.com>
2019-10-01 16:05:09 -0700

    Got a functioning server going. Added dependencies and middleware. Started on making a router and attaching functionality to it.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,23 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+const port = 3000;
 
+router.get('/', (req, res) => {
+  res.send('Home page');
+})
+
+app.post('/', (req, res) => (
+  res.send('POST successful')
+  ));
+
+app.listen(port, () =>
+  console.log('Listening on port ' + port)
+);
+
+app.use(express.static('client'));
+
+module.exports = router;
