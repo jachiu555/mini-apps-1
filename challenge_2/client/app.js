f6eac9b87b70c04403cb37e33a6d19c99c6d88aa
James Chiu <jachiu555@gmail.com>
2019-10-02 08:29:48 -0700

    Started working on front-end.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,60 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      body: {}
+    }
+
+    this.handlePOSTSubmit = this.handlePOSTSubmit.bind(this);
+
+  }
+
+  getPage() {
+    axios.get('/')
+      .then((response) => {
+        this.setState({
+          body: response.data
+        })
+      })
+      .catch((error) => {
+        console.log('ERROR', error);
+      })
+  }
+
+  handlePOSTSubmit(e) {
+    e.preventDefault();
+    axios.post('/', {
+      body: this.state.body
+    })
+      .then((response) => {
+        console.log('POSTED');
+      })
+      .then( =>  {
+        this.getPage();
+      })
+      .catch((error) => {
+        console.log('ERROR', error);
+      });
+
+    this.setState({
+      body: e.target.value, () => console.log(this.state.body);
+    })
+  }
 
 
+
+  render() {
+    return (
+      <div>
+         <form>
+              <input type="text" name="json" id="jsonID"/>
+              <input type="submit" value="Submit" onClick={this.handleSubmit}/>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App;
