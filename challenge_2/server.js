d4f9bfcc6f82f55d4dcec41d6c5bd07ceb7c4b3b
James Chiu <jachiu555@gmail.com>
2019-10-01 17:07:13 -0700

    Included the middleware and initialized them. Got access to req.body with inputs being sent to page.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,6 +3,12 @@ const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+app.use(express.static('client'));
 
 const port = 3000;
 
@@ -11,13 +17,11 @@ router.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => (
-  res.send('POST successful')
-  ));
+  res.send(req.body.json)
+));
 
 app.listen(port, () =>
   console.log('Listening on port ' + port)
 );
-
-app.use(express.static('client'));
 
 module.exports = router;
