539cc3165e8fda30e9dcfa4eb5156c721e7840a4
James Chiu <jachiu555@gmail.com>
2019-10-02 23:18:36 -0700

    Started working on front-end. Got a basic input form. Adding functionality.

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,25 @@
+import credentials from './credentials.jsx'
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '';
+    }
+  }
 
+  
+
+  render() {
+    return (
+      <div className="body">
+      <div className="heading">CheckOut</div>
+        <form>
+          <Credentials inputName={this.}
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App;
